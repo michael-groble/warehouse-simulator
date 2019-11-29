69bcd812d2c63b4177a42e7802fc73ec5f6bef48
michael groble <mike@groble.me>
2019-11-29 10:28:06 -0600

    make sure line is configured (members are hooked up with next-in-lines) and add ability to process a set of pick tickets

--- lib/warehouse_simulator/line.ex
+++ lib/warehouse_simulator/line.ex
@@ -7,27 +7,97 @@ defmodule WarehouseSimulator.Line do
   alias WarehouseSimulator.Picker
   alias WarehouseSimulator.Checker
 
+  @doc """
+  Constructs a list of `{module, [args]}` tuples from the specified file that can then be passed to `@start_link/1`:
+
+      members = Line.options_from_file("line.json")
+      Line.start_link(members, line_name)
+  """
   def options_from_file(filename) do
     ~w(members type parameters) |> Enum.each(&String.to_atom/1)
 
     with {:ok, body} <- File.read(filename),
          {:ok, json} <- Jason.decode(body, keys: :atoms!) do
-      json[:members]
-      |> Enum.with_index()
-      |> Enum.map(fn {member_spec, i} ->
+      Enum.map(json[:members], fn member_spec ->
         module =
           case member_spec[:type] do
             "Picker" -> Picker
             "Checker" -> Checker
           end
 
-        %{
-          id: i,
-          start:
-            {module, :start_link,
-             [struct(Module.concat(module, Parameters), member_spec[:parameters])]}
-        }
+        {module, [struct(Module.concat(module, Parameters), member_spec[:parameters])]}
       end)
     end
+  end
+
+  def start_link(members, name) do
+    result = Supervisor.start_link(__MODULE__, members, name: name)
+
+    with {:ok, pid} <- result do
+      link_children(pid)
+    end
+
+    result
+  end
+
+  def process_pick_tickets(line, pick_tickets) do
+    {member, module} = first_member(line)
+
+    Enum.reduce(pick_tickets, 0, fn pick_ticket, time ->
+      module.process_pick_ticket(member, time, pick_ticket, %{})
+    end)
+
+    times(line)
+  end
+
+  defp times(line) do
+    ordered_members(line)
+    |> Enum.map(fn {member, module} ->
+      %{elapsed: module.elapsed_time(member), idle: module.idle_time(member)}
+    end)
+  end
+
+  def init(members) do
+    children =
+      members
+      |> Enum.with_index()
+      |> Enum.map(fn {member, i} ->
+        Supervisor.child_spec(member, id: i)
+      end)
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  defp link_children(line) do
+    ordered_members(line)
+    |> List.foldr(nil, fn current, next ->
+      with {pid, module} <- current,
+           {next_pid, next_module} <- next do
+        module.get_and_put_next_line_member(pid, next_pid, next_module)
+      end
+
+      current
+    end)
+  end
+
+  # children ordered by `id` as `{pid, module}` tuples
+  defp ordered_members(line) do
+    Supervisor.which_children(line)
+    |> Enum.sort_by(&child_id/1)
+    |> Enum.map(&child_pid_and_module/1)
+  end
+
+  defp child_id(child) do
+    with {id, _, _, _} <- child, do: id
+  end
+
+  defp child_pid_and_module(child) do
+    with {_, pid, _, [module | _]} <- child, do: {pid, module}
+  end
+
+  defp first_member(line) do
+    Supervisor.which_children(line)
+    |> Enum.min_by(&child_id/1)
+    |> child_pid_and_module
   end
 end
--- test/warehouse_simulator/line_test.exs
+++ test/warehouse_simulator/line_test.exs
@@ -4,24 +4,36 @@ defmodule WarehouseSimulator.LineTest do
   alias WarehouseSimulator.Line
   alias WarehouseSimulator.Picker
   alias WarehouseSimulator.Checker
+  alias WarehouseSimulator.PickTicket
 
   doctest Line
 
   describe "options_from_file" do
     options = Line.options_from_file("test/fixtures/simple_line.json")
-    {%{id: id, start: {module, _init, [params | _]}}, options} = List.pop_at(options, 0)
-    assert id == 0
+    {{module, [params]}, options} = List.pop_at(options, 0)
     assert module == Picker
     assert params.pickable_items == ["A", "B"]
-    {%{id: id, start: {module, _init, [params | _]}}, options} = List.pop_at(options, 0)
-    assert id == 1
+    {{module, [params]}, options} = List.pop_at(options, 0)
     assert module == Picker
     assert params.pickable_items == ["C", "D"]
-    {%{id: id, start: {module, _init, [params | _]}}, options} = List.pop_at(options, 0)
-    assert id == 2
+    {{module, [params]}, options} = List.pop_at(options, 0)
     assert module == Checker
     assert params.check_probability == 0.25
     assert params.seconds_per_pick_ticket == 2.0
     assert length(options) == 0
+  end
+
+  describe "process_pick_tickets" do
+    # do the same thing as the Picker test "passes tickets along and accumulates time"
+    members = [
+      {Picker, %Picker.Parameters{pickable_items: ["A"], seconds_per_quantity: 1.0}},
+      {Picker, %Picker.Parameters{pickable_items: ["B"], seconds_per_quantity: 1.0}}
+    ]
+
+    pick_tickets = for _ <- 1..3, do: %PickTicket{item_picks: %{"A" => 1, "B" => 2}}
+
+    {:ok, line} = Line.start_link(members, :line)
+    times = Line.process_pick_tickets(line, pick_tickets)
+    assert times == [%{elapsed: 11.0, idle: 2.0}, %{elapsed: 15.0, idle: 3.0}]
   end
 end
